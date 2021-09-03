2df6f5a0981538fa2367d57debde4fa8fb9fdbbb
mtriner <matttriner@gmail.com>
2021-09-02 23:26:25 -0400

    adding

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+//test comment
 export default function App() {
   return (
     <View style={styles.container}>
